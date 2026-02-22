4950aa1a0bb01d1e90879131d12eecca3cc1a3d9
zachwesterberg01 <zachwesterberg01@gmail.com>
2026-02-22 18:52:30 -0500

    Ass3 small change

--- Assignments/Assignment3/processNames.php
+++ Assignments/Assignment3/processNames.php
@@ -2,31 +2,31 @@
 
 function addClearNames(): string
 {
-    // If Clear Names button clicked
+    /
     if (isset($_POST["clear"])) {
         return "";
     }
 
-    // Start with the existing list (textarea content)
+    
     $existingList = "";
     if (isset($_POST["namelist"])) {
         $existingList = trim($_POST["namelist"]);
     }
 
-    // Convert existing list into array
+   
     $namesArray = [];
     if ($existingList !== "") {
         $namesArray = explode("\n", $existingList);
     }
 
-    // If Add Name button clicked, add the new formatted name
+    
     if (isset($_POST["add"])) {
         $fullName = "";
         if (isset($_POST["fullname"])) {
             $fullName = trim($_POST["fullname"]);
         }
 
-        // Assignment note says we can assume "First Last" always
+        
         if ($fullName !== "") {
             $parts = explode(" ", $fullName);
             $first = $parts[0];
@@ -37,7 +37,7 @@ function addClearNames(): string
         }
     }
 
-    // Sort and convert back to one string with \n
+    
     sort($namesArray);
     return implode("\n", $namesArray);
 }
